103cd29a6bd172a9648803019f5c64d8ffce076a
dani_edo <dani.edogawa.17@gmail.com>
2024-10-16 10:55:29 +0700

    object detection (duck) using yolo 8

---
+++ main.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+config_path = 'config.yaml'
+
+# Load a model
+model = YOLO('yolov8n.pt')  # load pre trained model
+
+# Use the model
+model.train(data=config_path, epochs=10, batch=16)  # train the model (change epochs to 200 and batch to 32 for ideal condition)
